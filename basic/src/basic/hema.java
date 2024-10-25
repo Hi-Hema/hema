60232b3d5217c90e09143cee1c0a23b81b0ff737
DELL <DELL@DESKTOP-TJASO1D>
2024-10-25 10:46:13 +0530

    hi hello

---
+++ basic/src/basic/hema.java
@@ -0,0 +1,19 @@
+package basic;
+import java.util.*;
+public class hema {
+
+	public static void main(String[] args) {
+
+		 System.out.println("TNS JAVA DAY 1");
+			 System.out.println("Hi Hema!");
+			 int a,b,c;
+			 Scanner s=new Scanner(System.in);
+			 System.out.println("Enter the value of a and b");
+			 a=s.nextInt();
+			 b=s.nextInt();
+			 c=a+b;
+			 System.out.println("Addition of a="+a+" and b="+b+" is:"+c);
+
+	}
+
+}
